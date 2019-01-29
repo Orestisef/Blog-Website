cde766bf5509f939e50a1479b27f648a695c2a5b
norest <orestisef@gmail.com>
2019-01-29 18:50:18 +0200

    Adding header & footer

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render(__dirname + "/views/home.ejs");
+  res.render("home", {startingContent: homeStartingContent });
 });
 
 
